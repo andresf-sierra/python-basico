a95b75cccd1a068117ab7a4cdb4416532e9bbd78
Andres Sierra <andrew.lol.serra@gmail.com>
2022-02-03 22:28:45 -0500

    bmi modularizado

---
+++ bmi.py
@@ -0,0 +1,32 @@
+def run():
+
+    bmi = bmi_calculator()
+    print("Su índice de masa corporal (BMI) es de " + bmi)
+
+
+def bmi_calculator():
+
+    height = (input("Ingresa tu altura en metros (m): "))
+    height = float(height)
+
+    weight = input("Ingresa tu peso en kilogramos (Kg): ")
+    weight = float(weight)
+
+    bmi = weight / height ** 2
+    bmi = round(bmi, 2)
+    bmi = str(bmi)
+
+    return bmi
+   
+
+if __name__ == "__main__":
+    run()
+
+
+# height = input("Ingrese su altura: ")
+# height = float(height)
+# weight = input("Ingrese su peso: ")
+# weight = float(weight)
+# bmi = weight / height ** 2
+# bmi = str(bmi)
+# print(bmi)
